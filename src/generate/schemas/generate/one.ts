988989c9dffbf3aa8e82c2bc896facd1fb49a59e
Kristian Mandrup <kristian.mandrup@yeay.com>
2018-06-12 16:10:12 +0200

    generate better YeaY schemas and data

--- src/generate/schemas/generate/one.ts
+++ src/generate/schemas/generate/one.ts
@@ -25,6 +25,100 @@ function wrapAsArray(schema: any) {
   return $schema
 }
 
+const fakerMap = {
+  id: "random.uuid",
+  imageUrl: 'image.imageUrl',
+  name: 'commerce.productName',
+  description: 'lorem.words',
+  network: 'internet.domainName',
+  rank: {"random.number": [20]},
+  ranking: {"random.number": [20]},  
+  currency: "finance.currencyCode",
+  email: "internet.email",
+  firstname: "name.firstName",
+  "lastname": "name.lastName",
+  "username": "internet.userName",
+  locale: "random.locale",
+  "phoneNumber": "phone.phoneNumber",
+  website: 'internet.domainName',
+  brands: "company.companyName"
+}
+
+function addFakerToProp(key: string, prop: any) {  
+  if (/Id$/.test(key)) {
+    prop.faker = "random.number"
+  }
+
+  if (key === 'gender') {
+    prop.enum = [ "male", "female"]
+  }
+
+  if (key === 'videoType') {
+    prop.enum = [
+      "sale",
+      ""
+    ]
+  }
+  
+  if (/Currency$/.test(key)) {
+    prop.faker = "finance.currencyCode"
+  }
+  
+
+  if (/Likes$/.test(key) || /Views$/.test(key) || /Comments$/.test(key)) {
+    prop.faker = {"random.number": [20]}
+  }
+
+
+  if (/Code$/.test(key)) {
+    prop.faker = "lorem.slug"
+  }
+
+  if (/Url$/.test(key)) {
+    prop.faker = "internet.url"
+  }
+
+  if (/LogoUrl$/.test(key) || /ImageUrl$/.test(key) || /PreviewUrl$/.test(key) || /MediaUrl$/.test(key) || /mediaUrl$/.test(key) || /previewUrl$/.test(key) || /VideoUrl$/.test(key)) {
+    prop.faker = "image.avatar"
+  }
+
+  if (/Amount$/.test(key)) {
+    prop.faker = {"finance.amount": [10, 1000, 2]} // "random.number"
+  }
+
+  if (key === 'roles') {
+    prop.enum = [
+      'admin',
+      'guest',
+      'user'
+    ]
+  }
+  
+  if (/Score$/.test(key)) {
+    prop.faker = {"random.number": [1000]}
+  }
+
+  if (/At$/.test(key) || key === 'serverTime') {
+    prop.format = 'date-time'
+  }
+
+  if (prop.format === 'date-time') {
+    prop.faker = 'date.recent'
+  }
+
+  if (key === 'endDate') {
+    prop.format = 'date-time'
+    prop.faker = 'date.future'    
+  }
+
+  const faker = fakerMap[key]    
+  if (faker) {
+    prop.faker = faker
+  }
+
+  return prop
+}
+
 export async function generateOne(path : any, pathKey: string, doc: any) {
   // only GET methods are valid
   const methodName : any = ['get'].find((name : string) => !!path[name])
@@ -68,9 +162,15 @@ export async function generateOne(path : any, pathKey: string, doc: any) {
   }
 
   if (schema.items) {
-    const keys = Object.keys(schema.items.properties || {})
+    let properties = schema.items.properties || {}
+    const keys = Object.keys(properties)
     schema.uniqueItems = true
-    schema.items.required = keys  
+    schema.items.required = keys
+    
+    properties = keys.reduce((acc, key) => {
+      acc[key] = addFakerToProp(key, properties[key])
+      return acc
+    }, {})
   }
 
   const schemaFileName = `${dasherize(name)}.json`
