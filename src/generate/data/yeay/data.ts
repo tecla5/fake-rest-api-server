5d79596843541a87bdbb4b331b219099bd680f6d
Kristian Mandrup <kristian.mandrup@yeay.com>
2018-06-12 14:36:02 +0200

    remove debugging for data gen

--- src/generate/data/yeay/data.ts
+++ src/generate/data/yeay/data.ts
@@ -3,26 +3,13 @@ import {schemas} from '../../../../schemas/yeay';
 const _ = require('lodash');
 _.mixin(require("lodash-inflection"))
 
-const { log } = console
-log({
-  schemas
-})
-
 const schemaNames = Object.keys(schemas)
 
 export const data = schemaNames.reduce((acc: any, name : any) => {
-  log({
-    schema: name
-  })  
-
   const schema = schemas[name]
   const pluralModelName = _.pluralize(name)
   // pluralModelName !== name
-  if (true) {
-    log({
-      collection: name
-    })  
-  
+  if (true) {  
     // avoid overwrite
     if (!acc[pluralModelName]) {
       acc[pluralModelName] = jsf(schema);
